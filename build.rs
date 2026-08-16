b8a86dd7b7100fd1960bedc074cbf8c8e3aefb49
SpacingBat3 <spacingbat3@gmail.com>
2026-08-16 15:51:29 +0200

    refactor(build): no crust?

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
-use std::{env::var, os::{raw::{c_char, c_double}, unix::process::CommandExt}, process::Command};
+use std::{env::var, process::Command};
 
 fn main() {
     let target = var("CARGO_TARGET_DIR").unwrap_or("target".to_string());
     let profile = var("PROFILE").unwrap();
     let cc =  var("CC").unwrap_or("cc".to_string());
-    let _ = Command::new(cc)
+    Command::new(cc)
         .args([
             "-o", format!("{}/{}/noncrust", target, profile).as_str(),
             "src/compare.c"
         ])
-        .exec();
+        .spawn().unwrap()
+        .wait().unwrap();
 }

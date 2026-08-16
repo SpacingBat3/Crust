81d0b96a90d2baada95e46d234daf9f440654bc6
SpacingBat3 <spacingbat3@gmail.com>
2026-08-16 15:09:06 +0200

    fix: more crust in rust

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ fn panic(_info:&PanicInfo) -> ! { unsafe { libc::abort(); } }
 #[unsafe(no_mangle)]
 fn rust_eh_personality() {}
 
-use libc::printf;
+use libc::{
+    printf,
+    size_t
+};
 use core::ffi::*;
 
 #[unsafe(no_mangle)]
@@ -16,7 +19,7 @@ unsafe extern "C" fn main(argc:c_int, argv: *const *const c_char)->c_int { unsaf
     printf(c"Hello world in Crust!\n".as_ptr());
     if argc > 1 {
         printf(c"You can even print your argv:\n".as_ptr());
-        for i in 1usize..(argc as usize) {
+        for i in (1 as size_t)..(argc as size_t) {
             printf(c" * %s\n".as_ptr(),*argv.add(i));
         }
         printf(c":D\n".as_ptr());
